8f12c62c32fe2c6045546bd67737b3baad07d538
Ulices Meléndez Acosta <ulices009@hotmail.com>
2017-09-02 19:00:34 -0500

    Primera versión.

---
+++ NLP_PruebaConcepto/Constants/ExcecutablePaths.cs
@@ -0,0 +1,6 @@
+namespace NLP_PruebaConcepto.Constants {
+
+    public static class ExcecutablePaths {
+        public static string MicrosoftWord = "C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE";
+    }
+}
